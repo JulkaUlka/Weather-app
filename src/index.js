9b15ad3cbb53cd3a2e2c2d606f948ba7a2dd7ba9
JulkaUlka <y.glushko@outlook.com>
2022-07-26 13:55:11 +0300

    made API forecast

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// display current day
+
 let currentDay = new Date();
 let days = [
   "Sunday",
@@ -13,6 +15,8 @@ let nowDate = currentDay.getDate();
 let nowDay = days[currentDay.getDay()];
 let currentDayShow = document.querySelector("div#currentDate");
 currentDayShow.innerHTML = `${nowDay}, ${nowDate}`;
+
+// display weather for current day
 
 function displayWeatherCondition(response) {
   document.querySelector("#cityName").innerHTML = response.data.name;
@@ -33,6 +37,8 @@ function displayWeatherCondition(response) {
   sunriseElement.innerHTML = formateDate(response.data.sys.sunrise * 1000);
   let sunsetElement = document.querySelector("#sunset");
   sunsetElement.innerHTML = formateDate(response.data.sys.sunset * 1000);
+
+  getForecast(response.data.coord);
 }
 function formateDate(timestamp) {
   let date = new Date(timestamp);
@@ -46,6 +52,9 @@ function formateDate(timestamp) {
   }
   return `${hours}:${minutes}`;
 }
+
+// search city and position
+
 function searchCity(city) {
   let apiKey = "ff425fafca53dfa00a8edad5ed4545b5";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -63,29 +72,6 @@ function cityValue(event) {
 let citySearch = document.querySelector("#searchCity");
 citySearch.addEventListener("keypress", cityValue);
 
-function fTemp(event) {
-  event.preventDefault();
-  celcius.classList.remove("active");
-  fahrenheit.classList.add("active");
-
-  let temp = document.querySelector("#display-temp");
-  let displayTemp = temp.innerHTML;
-  temp.innerHTML = Math.round((displayTemp * 9) / 5 + 32);
-}
-let tempChangeForF = document.querySelector("#fahrenheit");
-tempChangeForF.addEventListener("click", fTemp);
-
-function cTemp(event) {
-  event.preventDefault();
-  celcius.classList.add("active");
-  fahrenheit.classList.remove("active");
-  let temp = document.querySelector("#display-temp");
-  let displayTemp = temp.innerHTML;
-  temp.innerHTML = Math.round((5 / 9) * (displayTemp - 32));
-}
-let tempChangeForC = document.querySelector("#celcius");
-tempChangeForC.addEventListener("click", cTemp);
-
 function searchLocation(position) {
   let apiKey = "ff425fafca53dfa00a8edad5ed4545b5";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
@@ -101,3 +87,71 @@ let geolocation = document.querySelector("#searchPosition");
 geolocation.addEventListener("click", getCurrentLocation);
 
 searchCity("Kyiv");
+
+// convert temperature
+
+function fTemp(event) {
+  event.preventDefault();
+  celcius.classList.remove("active");
+  fahrenheit.classList.add("active");
+
+  let temp = document.querySelector("#display-temp");
+  let displayTemp = temp.innerHTML;
+  temp.innerHTML = Math.round((displayTemp * 9) / 5 + 32);
+}
+
+let tempChangeForF = document.querySelector("#fahrenheit");
+tempChangeForF.addEventListener("click", fTemp);
+
+function cTemp(event) {
+  event.preventDefault();
+  celcius.classList.add("active");
+  fahrenheit.classList.remove("active");
+  let temp = document.querySelector("#display-temp");
+  let displayTemp = temp.innerHTML;
+  temp.innerHTML = Math.round((5 / 9) * (displayTemp - 32));
+}
+let tempChangeForC = document.querySelector("#celcius");
+tempChangeForC.addEventListener("click", cTemp);
+
+// display forecast by days
+
+function getForecast(coordinates) {
+  let apiKey = "ff425fafca53dfa00a8edad5ed4545b5";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function formatForecastDays(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = document.querySelector("#forecast");
+  let dailyForecast = response.data.daily;
+  let forecastHTML = `<div class="row row-cols-md-6">`;
+  dailyForecast.forEach(function (days, index) {
+    if (index < 7 && index > 0) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col">
+          <div class="card">
+            <h5 class="card-title">${formatForecastDays(days.dt)}</h5>
+            <img src="https://openweathermap.org/img/wn/${
+              days.weather[0].icon
+            }@2x.png" />
+            <div class="card-body">
+              <p class="card-text"><strong>${Math.round(
+                days.temp.max
+              )}°</strong>/${Math.round(days.temp.min)}°</p>
+            </div>      
+            </div>
+          </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecast.innerHTML = forecastHTML;
+}
